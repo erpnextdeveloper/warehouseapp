8384d891172d429a6b455b0072efbcc72c7724bc
root <root@vultr.guest>
2018-09-27 10:46:20 +0000

    2nd

---
+++ warehouseapp/warehouseapp/doctype/packing_slips/packing_slips.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, wareshouseapp and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Packing Slips', {
+	refresh: function(frm) {
+
+	}
+});
